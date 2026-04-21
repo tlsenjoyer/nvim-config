b621d6bc06fdcbd9c574d302ffb230ecc1063540
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2026-04-22 00:31:46 +0300

    fix having to comment out telescope mapping on a fresh install

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,36 +1,38 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      sorting_strategy = "ascending",
-      layout_config = {
-        prompt_position = "top",
-      },
-      file_ignore_patterns = {
-        ".git", ".next", "node_modules", "target"
-      },
-      prompt_prefix = "",
-      mappings = { -- comment out mappings when reinstalling config, then comment back in after telescope installs
-        n = {
-          ["q"] = require("telescope.actions").close,
-          ["<C-d>"] = require("telescope.actions").delete_buffer,
+  cmd = "Telescope",
+  opts = function()
+    local actions = require("telescope.actions")
+
+    return {
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+          prompt_position = "top",
         },
-      }
-    },
-    pickers = {
-      find_files = {
-        find_command = {
-          "rg",          -- ripgrep
-          "--no-ignore", -- disable exclusion of files/directories based on .gitignore
-          "--files",     -- dunno
-          "--hidden"     -- search hidden files/directories (ones that begin with a dot)
+        file_ignore_patterns = {
+          ".git", ".next", "node_modules", "target"
+        },
+        prompt_prefix = "",
+        mappings = {
+          n = {
+            ["q"] = actions.close,
+            ["<C-d>"] = actions.delete_buffer,
+          },
         },
       },
-    },
-  },
+      pickers = {
+        find_files = {
+          find_command = {
+            "rg",          -- ripgrep
+            "--no-ignore", -- disable exclusion of files/directories based on .gitignore
+            "--files",     -- dunno
+            "--hidden"     -- search hidden files/directories (ones that begin with a dot)
+          },
+        },
+      },
+    }
+  end,
   dependencies = { "nvim-lua/plenary.nvim" },
   branch = "0.1.x",
-  keys = {
-    { "gd", ":Telescope lsp_definitions<CR>", { nowait = true } },
-  },
 }
