cd00beffecd806d4f8ed2586edf52d5363b5e2e6
Morozov Matvey <298788116+tlsenjoyer@users.noreply.github.com>
2026-07-08 20:05:42 +0200

    Update comment.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -17,17 +17,30 @@ return {
       mode = "v",
     },
   },
-  opts = {
-    toggler = {
-      line = "<leader>/",
-      block = "<nop>",
-    },
-    opleader = {
-      line = "<leader>/",
-      block = "<leader>b/",
-    },
-    mappings = {
-      extra = false, -- don't create unnecessary mappings
+  opts = function()
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    local ft = require("Comment.ft")
+
+    return {
+      toggler = {
+        line = "<leader>/",
+        block = "<nop>",
+      },
+      opleader = {
+        line = "<leader>/",
+        block = "<leader>b/",
+      },
+      mappings = {
+        extra = false, -- don't create unnecessary mappings
+      },
+      pre_hook = function(ctx)
+        local ok, commentstring = pcall(ts_context_commentstring, ctx)
+        if ok and commentstring then
+          return commentstring
+        end
+
+        return ft.get(vim.bo.filetype, ctx.ctype) or vim.bo.commentstring
+      end,
     }
-  }
+  end,
 }
